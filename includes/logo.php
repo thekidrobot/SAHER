eed1e7584f1c9b0ac9f849caf856ab2edc0b9320
Andres Gonzalez <andgonzalez81@gmail.com>
2011-08-20 12:07:35 +0530

    Graphical work done

--- includes/logo.php
+++ includes/logo.php
@@ -7,14 +7,12 @@
       <?php if ($curr_page == "partners.php") {?><div id="logo"><img src="images/partners_banner.png"></div><?php } ?>
       <?php if ($curr_page == "strategy.php") {?><div id="logo"><img src="images/strategies_banner.png"></div><?php } ?>
       <?php if ($curr_page == "our_work.php") {?><div id="logo"><img src="images/our_work_banner.png"></div><?php } ?>
-      <?php if ($curr_page == "programs.php"
-                or $curr_page == "neenv.php"
-                or $curr_page == "mhamara.php"
-                or $curr_page == "tameer.php"
+      <?php if ($curr_page == "neenv.php" or $curr_page == "tameer.php") {?><div id="logo"><img src="images/neenv_banner.png"></div><?php } ?>
+      <?php if ($curr_page == "mhamara.php"
                 or $curr_page == "ccenter.php"
                 or $curr_page == "governance.php"
-                or $curr_page == "peace_week.php")
-                {?><div id="logo"><img src="images/programs_banner.png"></div><?php } ?>
+                or $curr_page == "peace_week.php") {?><div id="logo"><img src="images/mhamara_banner.png"></div><?php } ?>
+      <?php if ($curr_page == "programs.php") {?><div id="logo"><img src="images/programs_banner.png"></div><?php } ?>
       <?php if ($curr_page == "our_stories.php") {?><div id="logo"><img src="images/stories_banner.png"></div><?php } ?>
       <?php if ($curr_page == "get_involved.php") {?><div id="logo"><img src="images/get_involved_banner.png"></div><?php } ?>
       <?php if ($curr_page == "contact.php") {?><div id="logo"><img src="images/main_banner.png"></div><?php } ?>
